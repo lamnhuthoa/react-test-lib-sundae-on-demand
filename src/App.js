d3420242552a3fa41f41431f9173b1da4f4edf52
Lam Nhut Hoa (CI/XDV1) <Hoa.LamNhut@vn.bosch.com>
2023-07-16 23:56:58 +0700

    Remove src/App.test.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-// import { useState } from 'react';
 import Container from "react-bootstrap/Container";
 import OrderEntry from "./pages/entry/OrderEntry";
 import { OrderDetailsProvider } from "./contexts/OrderDetails";
-// export function replaceCamelWithSpaces(colorName) {
-//   return colorName.replace(/\B([A-Z])\B/g, ' $1');
-// }
 
 function App() {
-  // const [buttonColor, setButtonColor] = useState('MediumVioletRed');
-  // const [disabled, setDisabled] = useState(false);
-  // const newButtonColor = buttonColor === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed';
-  // const grayButtonColor = disabled ? 'gray' : buttonColor
-
-  // const handleChangeButton = () => {
-  //   setButtonColor(newButtonColor);
-  // }
-
   return (
-    // <div>
-    //   <button
-    //     style={{ backgroundColor: grayButtonColor, color: "#fff" }}
-    //     onClick={handleChangeButton}
-    //     disabled={disabled}
-    //   >
-    //     Change to {replaceCamelWithSpaces(newButtonColor)}
-    //   </button>
-    //   <br/>
-    //   <input
-    //     type='checkbox'
-    //     id="disable-button-checkbox"
-    //     defaultChecked={disabled}
-    //     onChange={(e) => setDisabled(e.target.checked)}
-    //   />
-    //   <label htmlFor='disable-button-checkbox'>Disable button</label>
-    // </div>
     <Container>
       <OrderDetailsProvider>
         {/* Summary page and entry page need provider */}
--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <SummaryForm /> */}
-    {/* <OrderEntry /> */}
   </React.StrictMode>
 );
 
